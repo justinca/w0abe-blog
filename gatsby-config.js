f9f33ba5ad0de6c684a63c1eb668677f336b8964
Justin Calvert <justin.m.calvert@gmail.com>
2022-01-17 15:38:47 -0700

    pwa 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,13 +114,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `W0ABE.com`,
-        short_name: `GatsbyJS`,
+        name: `W0̷ABE.com`,
+        short_name: `W0̷ABE`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `src/images/tower.png`, // This path is relative to the root of the site.
       },
